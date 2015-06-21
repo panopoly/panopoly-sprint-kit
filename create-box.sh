8bcd6c603ba7aff8b7489382478dda0fe3db73ff
Matt Glaman <nmd.matt@gmail.com>
2015-06-21 11:19:12 -0500

    Create desktop VM environment, server environment to Vagrant.

--- create-box.sh
+++ create-box.sh
@@ -2,7 +2,31 @@
 
 set -e
 
-rm -rf output-virtualbox-iso || true
-git submodule update
-packer validate kit-box.json
-packer build kit-box.json
+clean() {
+  rm -rf output-virtualbox-iso || true
+  git submodule update
+}
+
+desktop_build() {
+  clean
+  packer validate kit-box-desktop.json
+  packer build kit-box-desktop.json
+}
+
+vagrant_build() {
+  clean
+  packer validate kit-box-vagrant.json
+  packer build kit-box-vagrant.json
+}
+
+case $1 in
+  "--help| -h")
+    echo "Builds Panopoly kit boxes. Use desktop or vagrant as an argument"
+    ;;
+  "desktop")
+    desktop_build
+    ;;
+  "vagrant")
+    vagrant_build
+    ;;
+esac
