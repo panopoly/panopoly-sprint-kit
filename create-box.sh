e9eee8b7b213fd5e8d5a9a4400c04cc62fb4575a
Matt Glaman <nmd.matt@gmail.com>
2015-06-22 07:45:36 -0500

    Update Desktop and Vagrant build process, reduce size.
    Desktop still isn't installing GUI.

--- create-box.sh
+++ create-box.sh
@@ -4,19 +4,29 @@ set -e
 
 clean() {
   rm -rf output-virtualbox-iso || true
-  git submodule update
+  git submodule update --init
 }
 
 desktop_build() {
   clean
   packer validate kit-box-desktop.json
   packer build kit-box-desktop.json
+  if [ $? -ne 0 ]; then
+    notify-send "Panopoly kit desktop failed to build"
+  else
+    notify-send "Panopoly kit desktop built"
+  fi
 }
 
 vagrant_build() {
   clean
   packer validate kit-box-vagrant.json
   packer build kit-box-vagrant.json
+  if [ $? -ne 0 ]; then
+    notify-send "Panopoly kit vagrant box failed to build"
+  else
+    notify-send "Panopoly kit varant box built"
+  fi
 }
 
 case $1 in
