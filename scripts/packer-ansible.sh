1608b38c2adaf1d0742cb10ded7ef446b959eb4e
Matt Glaman <nmd.matt@gmail.com>
2015-06-23 02:42:53 -0500

    Move UI setup to Ansible role for the kit.

--- scripts/packer-ansible.sh
+++ scripts/packer-ansible.sh
@@ -2,9 +2,11 @@
 
 # Install Ansible repository.
 apt-get -y update && apt-get -y upgrade
+
 apt-get -y install software-properties-common
 apt-add-repository ppa:ansible/ansible
 
 # Install Ansible.
 apt-get -y update
 apt-get -y install ansible
+
