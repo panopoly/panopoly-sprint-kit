838a718924dbc859bd634090c0f55a5afe1e5f23
David Snopek <dsnopek@gmail.com>
2015-07-01 13:29:36 -0500

    Successfully built vagrant base box!

--- scripts/packer-setup-vagrant.sh
+++ scripts/packer-setup-vagrant.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "panopoly        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+echo "vagrant         ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
