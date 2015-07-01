1735a28afe5a12b1f32a1740484a1047cdc88504
David Snopek <dsnopek@gmail.com>
2015-07-01 10:23:28 -0500

    Hrm. Ok, really attempt to get Ansible to run.

--- scripts/packer-setup-vagrant.sh
+++ scripts/packer-setup-vagrant.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+echo "panopoly        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
